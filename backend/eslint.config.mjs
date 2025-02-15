38fc6e7c885fd6abc187f1f6340cee1f0b90fd7a
Terrence Hyland <terrhy999@localhost.localdomain>
2025-02-15 15:57:52 -0500

    configure monorepo structure with eslint, typescript, and prettier

---
+++ backend/eslint.config.mjs
@@ -0,0 +1,23 @@
+// @ts-check
+
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier";
+
+export default tseslint.config(
+  {
+    ignores: ["eslint.config.mjs"]
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  ...tseslint.configs.strictTypeChecked,
+  eslintConfigPrettier,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+);
